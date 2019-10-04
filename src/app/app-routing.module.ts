1d7d892990ec9f791ab6c2a0110d53d55cd4becb
pejvak61 <a.mohammadi.t@live.com>
2019-10-04 14:32:38 +0330

    add about and page not found component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './cc/home/home.component';
+import { AboutComponent } from './cc/about/about.component';
+import { PageNotFoundComponent } from './cc/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: 'home'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './cc/home/home.component';
+import { AboutComponent } from './cc/about/about.component';
+import { PageNotFoundComponent } from './cc/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
