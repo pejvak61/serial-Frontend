8ab3c1039f183a579d9ab503a1a28004a97393b0
pejvak61 <a.mohammadi.t@live.com>
2019-10-04 19:03:28 +0330

    app.routing.module changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './cc/home/home.component';
 import { AboutComponent } from './cc/about/about.component';
 import { PageNotFoundComponent } from './cc/page-not-found/page-not-found.component';
+import { SerialreadComponent } from './cc/serialread/serialread.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'serialRead', component: SerialreadComponent},
   {path: '', component: HomeComponent},
-  {path: '', redirectTo: 'home'},
   {path: '**', component: PageNotFoundComponent}
 ];
 
