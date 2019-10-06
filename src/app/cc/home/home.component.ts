cddc82c11c0bbbbd6f8bc3a48b89611bde922e40
pejvak61 <a.mohammadi.t@live.com>
2019-10-06 06:54:26 +0330

    plan mousedown event for navigation to login page

--- src/app/cc/home/home.component.ts
+++ src/app/cc/home/home.component.ts
@@ -8,16 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  counter: number;
+  interval;
 
   constructor(public dataserv: DataServiceService,
               public routes: Router
-              ) { }
+              ) {
+                this.counter = 5;
+              }
   ngOnInit() {
   }
 
   public navTo(path: string) {
     if ((path !== '') || (path === undefined)) {
-      this.routes.navigateByUrl('home/' + path);
+      this.routes.navigateByUrl(path);
     }
+  }
+
+  public reverseCount(startsFrom: number = 5, endsTo: number = 0) {
+    this.counter = startsFrom;
+  }
+
+  public countDown(startsFrom: number = 5, endsTo: number = 0) {
+    this.counter = startsFrom;
+    this.interval = setInterval(() => {
+      this.counter--;
+      if (this.counter === 0) {
+        this.navTo('login');
+      }
+    }, 1000);
+  }
+
+  public stopCountingDown() {
+    this.counter = 5;
+    clearInterval(this.interval);
   }
 }
